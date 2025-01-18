7750f5c388987db28e31b7a48f3fb4b3cfaa5f46
Rohan Acharya <rohancharya42@gmail.com>
2025-01-18 16:17:51 +0545

    add the required file for building the system

---
+++ index.js
@@ -0,0 +1,16 @@
+class HashMap {
+  constructor(loadFactor, capacity) {
+    this.loadFactor = loadFactor;
+    this.capacity = capacity;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+
+    return hashCode;
+  }
+}
